2bbeafed65b67139ea53a44392f54f33d7106dc8
viet <viet.tranhoang@powergatesoftware.com>
2017-08-27 23:09:26 +0700

    add google login

--- camera_homework/src/pages/login-page/login-page.ts
+++ camera_homework/src/pages/login-page/login-page.ts
@@ -5,7 +5,8 @@ import { DataService } from './../../providers/data-service';
 import { RegisterPage } from './../register-page/register-page';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { GooglePlus } from '@ionic-native/google-plus';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,12 +16,14 @@ import { NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-login-page',
   templateUrl: 'login-page.html',
+  providers: [Facebook, GooglePlus]
 })
 export class LoginPage {
-email:string = "tranhoangviet2412@gmail.com";
-password : string = "123123";
-  constructor(public navCtrl: NavController, public navParams: NavParams,private _dataService : DataService,private _utility : UtilityService) {
-    
+  email: string = "tranhoangviet2412@gmail.com";
+  password: string = "123123";
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _dataService: DataService,
+    private _utility: UtilityService, private fb: Facebook, private googlePlus: GooglePlus) {
+    console.log(this.fb);
   }
 
   ionViewDidLoad() {
@@ -30,17 +33,33 @@ password : string = "123123";
     this.navCtrl.push(RegisterPage)
   }
   login(email, password) {
-    let data = { email: email, password: password,app_version: "1.0.0" }
-    this._dataService.login(data).subscribe(res => {     
+    let data = { email: email, password: password, app_version: "1.0.0" }
+    this._dataService.login(data).subscribe(res => {
       if (res.status == SystemConstants.STATUS_ERROR) {
         this._utility.alert('Login fail', res.message);
       } else {
-        localStorage.setItem('user',JSON.stringify(res.data));
+        localStorage.setItem('user', JSON.stringify(res.data));
         this.navCtrl.push(CameraPage).then(() => {
           const index = this.navCtrl.getActive().index;
           this.navCtrl.remove(0, index);
         });
       }
     })
+  }
+  loginFacebook() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then((res: FacebookLoginResponse) => {
+        this.navCtrl.push(CameraPage).then(() => {
+          const index = this.navCtrl.getActive().index;
+          this.navCtrl.remove(0, index);
+        });
+      }
+      )
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
+  loginGoogle() {
+    this.googlePlus.login({})
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
   }
 }
